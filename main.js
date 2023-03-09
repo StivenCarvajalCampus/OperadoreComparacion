d04e5f06921d4036e7849757f6f325574b7685f8
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-09 08:36:22 -0500

    feat(OperadoresComparacion): Compara si dos valores son iguales y del mismo tipo.
    Compara si dos valores son iguales y del mismo tipo.

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-//Igualdad (==): Compara si dos valores son iguales sin tener en cuenta su tipo. 
-igual = 5==5 // devuelve true
-console.log(igual)
+//Igualdad estricta (===): Compara si dos valores son iguales y del mismo tipo. 
+estricta= 5 === "5"//devuelve false
+console.log(estricta)
