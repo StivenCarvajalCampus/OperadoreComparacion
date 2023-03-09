56eb422ee1005bf46cc093b4d112841029fda384
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-09 09:13:43 -0500

    feat(OperadoresComparacion): :sparkles: AND lógico
    Devuelve verdadero si ambas expresiones son verdaderas

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//Mayor que (>), mayor o igual que (>=), menor que (<) y menor o igual que (<=): Comparan si un valor es mayor, mayor o igual, menor o menor o igual que otro valor. Por ejemplo:
-console.log(5 > 3 )// Devuelve true
-console.log(5 >= 5) // Devuelve true
-console.log(5 < 10) // Devuelve true
-console.log(5 <= 5) // Devuelve true
+//AND lógico (&&): Devuelve verdadero si ambas expresiones son verdaderas. 
+console.log(true && true )// Devuelve true
+console.log(true && false) // Devuelve false
