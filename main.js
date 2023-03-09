4e751f1c26d9b52c3110272d9b23b3d26fec5088
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-09 09:07:54 -0500

    feat(OperadoresComparacion): :sparkles: Mayor que, Mayor o igual, menor que, menor o igual

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-//Desigualdad estricta (!==): Compara si dos valores no son iguales y no son del mismo tipo. Por ejemplo:
-console.log (5 !== "6" )// Devuelve true
+//Mayor que (>), mayor o igual que (>=), menor que (<) y menor o igual que (<=): Comparan si un valor es mayor, mayor o igual, menor o menor o igual que otro valor. Por ejemplo:
+console.log(5 > 3 )// Devuelve true
+console.log(5 >= 5) // Devuelve true
+console.log(5 < 10) // Devuelve true
+console.log(5 <= 5) // Devuelve true
